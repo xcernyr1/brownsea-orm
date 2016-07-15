da7e848a2962d99adc6510d9a3fefc4d8aadaa40
Paul Probinson <probinson@nextfaze.com>
2016-07-15 13:27:50 +0930

    Removed unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+
+
 import { scoutsOauthBuilder } from './oauth-builder';
 import * as dotenv from 'dotenv';
 import * as request from 'request';
 
 var OAuth = require('oauth').OAuth;
-var open = require('opn');
+
 dotenv.config({silent: true});
 
 export * from './oauth-connection';
