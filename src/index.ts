1c080eb088fd64240ca3fad798b127bfb0a989c9
Paul Probinson <probinson@nextfaze.com>
2016-08-01 17:36:45 +0930

    remove dotenv from index

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import { scoutsOauthBuilder } from './oauth-builder';
 import * as dotenv from 'dotenv';
 import * as request from 'request';
 
-dotenv.config({silent: true});
-
 export * from './oauth-connection';
 export * from './mock-oauth';
 export * from './mock-request';
