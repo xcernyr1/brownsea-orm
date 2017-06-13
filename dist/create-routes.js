1e82de67392b0270f354dbe616097bde82dd10e6
probinson <probinson@snuvs.lan>
2017-06-13 14:51:31 +0930

    fix

--- dist/create-routes.js
+++ dist/create-routes.js
@@ -1,20 +1,21 @@
 "use strict";
 var url = require("url");
+
 function createRoutes(app, options) {
-    app.get('/auth/account', function (req, res, next) {
-        var _redirect = url.format({
-            protocol: options.protocool,
-            port: options.port,
-            hostname: options.hostname,
-            query: {
-                created: Date.now(),
-                id: req.query['access-token'],
-                userId: req.query['user-id']
-            }
-        });
-        res.redirect(_redirect);
-        delete req.session.passport;
+  app.get('/auth/account', function (req, res, next) {
+    var _redirect = url.format({
+      protocol: options.protocol,
+      port: options.port,
+      hostname: options.hostname,
+      query: {
+        created: Date.now(),
+        id: req.query['access-token'],
+        userId: req.query['user-id']
+      }
     });
+    res.redirect(_redirect);
+    delete req.session.passport;
+  });
 }
 exports.createRoutes = createRoutes;
 //# sourceMappingURL=create-routes.js.map
--- src/create-routes.ts
+++ src/create-routes.ts
@@ -1,30 +1,29 @@
 import * as url from 'url';
 
 export function createRoutes(app, options) {
-    // app.get('/', function (req, res, next) {
-    //     res.render('pages/index', {
-    //         user: req.user,
-    //         url: req.url,
-    //     });
-    // });
-    app.get('/auth/account', (req, res, next) => {
-        let _redirect = url.format({
-            protocol: options.protocool,
-            port: options.port,
-            hostname: options.hostname,
-            query: {
-                created: Date.now(),
-                id: req.query['access-token'],
-                userId: req.query['user-id']
-            }
-        })
-        res.redirect(_redirect)
-        delete req.session.passport;
-    });
-    // app.get('/login', function (req, res, next) {
-    //     res.render('pages/login', {
-    //         user: req.user,
-    //         url: req.url,
-    //     });
-    // });
+  // app.get('/', function (req, res, next) {
+  //     res.render('pages/index', {
+  //         user: req.user,
+  //         url: req.url,
+  //     });
+  // });
+  app.get('/auth/account', (req, res, next) => {
+    let _redirect = url.format({
+      protocol: options.protocol,
+      port: options.port,
+      hostname: options.hostname,
+      query: {
+        created: Date.now(),
+        id: req.query['access-token'],
+        userId: req.query['user-id']
+      }
+    })
+    res.redirect(_redirect) delete req.session.passport;
+  });
+  // app.get('/login', function (req, res, next) {
+  //     res.render('pages/login', {
+  //         user: req.user,
+  //         url: req.url,
+  //     });
+  // });
 }
